9a331047c0f9b641066f4d2ed3248800e2d48d86
Ochoja <danielochoja@yahoo.com>
2025-12-28 05:57:38 +0100

    updated contact info

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -53,7 +53,7 @@ import logo from '@/assets/images/whitelogo.png';
             info@meskeyprecisioneng.org
           </a>
           <!-- <a>+234 (0) 802 872 0639</a> -->
-          <a href="tel:+2348032827341">+234 (0) 803 282 7341</a>
+          <!-- <a href="tel:+2348032827341">+234 (0) 803 282 7341</a> -->
           <a href="tel:+2347030399465">+234 (0) 703 039 9465</a>
         </div>
       </div>
